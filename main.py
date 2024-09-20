dd00f3db0f464fd6a36dc47c4d1645cffa32cede
Hacker-web-Vi <mycryptobit99@gmail.com>
2024-09-20 18:57:27 +0200

    Enable CORS

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from app.models.error_models import ErrorResponseModel
 from app.routes.cosmos.staking.staking import staking_router
@@ -7,7 +8,6 @@ from app.routes.cosmos.slashing.slashing import slashing_router
 from app.routes.cosmos.bank.bank import bank_router
 from app.routes.cosmos.mint.mint import mint_router
 from app.routes.cosmos.distribution.distribution import distribution_router
-
 from utils.config import config
 
 app = FastAPI(docs_url=config.App.swagger_path if config.App.swagger else None,
@@ -15,6 +15,14 @@ app = FastAPI(docs_url=config.App.swagger_path if config.App.swagger else None,
               version=config.App.version,
               title=config.App.swagger_title,
               description=config.App.swagger_description)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["GET"],
+    allow_headers=["Content-Type"],
+)
 
 app.include_router(staking_router)
 app.include_router(upgrade_router)
